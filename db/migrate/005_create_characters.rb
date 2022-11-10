abfc69c3d0fc6ff090e94f38eddcb02cd27e2f36
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-10 20:00:06 +0300

    Initial

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_table :shows do |t|
+      t.string :name
+      t.string :actor_id
+      t.string :show_id
+    end
   end
 end
